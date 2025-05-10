fa7ff53e80931e10c1d501f325187ade493ccaf5
Zack ElMet <58562274+zelmetennani@users.noreply.github.com>
2025-05-11 00:34:08 +0900

    Add new equation image URL to equations array

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ if (typeof THREE === "undefined") {
     {"image": "https://latex.codecogs.com/png.latex?%5Cfrac%7B%5Cpi%7D%7B2%7D%20%3D%20%5Csum_%7Bn%3D0%7D%5E%5Cinfty%20%5Cfrac%7B(2n)!!%7D%7B(2n%2B1)!!%7D", "name": "Continued fraction for \u03c0"},
     {"image": "https://latex.codecogs.com/png.latex?%5Cfrac%7B%5Cpi%7D%7B4%7D%20%3D%20%5Carctan%5Cleft(%5Cfrac%7B1%7D%7B2%7D%5Cright)%20%2B%20%5Carctan%5Cleft(%5Cfrac%7B1%7D%7B3%7D%5Cright)", "name": "Arctangent identity"},
     {"image": "https://latex.codecogs.com/png.latex?%5Cfrac%7B1%7D%7B%5CGamma(z)%7D%20%3D%20z%20e%5E%7B%5Cgamma%20z%7D%20%5Cprod_%7Bn%3D1%7D%5E%7B%5Cinfty%7D%20%5Cleft(1%20%2B%20%5Cfrac%7Bz%7D%7Bn%7D%20%5Cright)%20e%5E%7B-z%2Fn%7D", "name": "Gamma function reciprocal"},
-    {"image": "https://latex.codecogs.com/png.latex?%5Cfrac%7B1%7D%7B%5Cpi%7D%20%3D%20%5Cfrac%7B2%5Csqrt%7B2%7D%7D%7B9801%7D%20%5Csum_%7Bk%3D0%7D%5E%5Cinfty%20%5Cfrac%7B(4k)!%281103%2B26390k%29%7D%7B(k!)%5E4%20396%5E%7B4k%7D%7D", "name": "Ramanujan's \u03c0 formula"}];
+    {"image": "https://latex.codecogs.com/png.latex?%5Cfrac%7B1%7D%7B%5Cpi%7D%20%3D%20%5Cfrac%7B2%5Csqrt%7B2%7D%7D%7B9801%7D%20%5Csum_%7Bk%3D0%7D%5E%5Cinfty%20%5Cfrac%7B(4k)!%281103%2B26390k%29%7D%7B(k!)%5E4%20396%5E%7B4k%7D%7D", "name": "Ramanujan's \u03c0 formula"},
+    {"image": "https://latex.codecogs.com/png.latex?%5Cfrac%7Bd%5En%7D%7Bdx%5En%7D%20(x%5En%20%5Cln%20x)%20%3D%20n!%20%5Cln%20x%20%2B%20n!%20H_n", "name": "Derivative of log powers"}];
   
     // Card geometry and material
     const cardGeometry = new THREE.BoxGeometry(1, 1, 1); // Equal dimensions for a cube
